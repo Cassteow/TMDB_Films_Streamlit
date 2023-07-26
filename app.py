2241ace2fa92b656bfcd7f0ad3ff349bb1dcd991
Cassandrea Teow <cassteowthg@gmail.com>
2023-07-26 23:48:55 +0800

    rearrange structure

--- app.py
+++ app.py
@@ -33,31 +33,34 @@ if result:
         response = conn.get_popular_movies()
     elif movie_type == "Top Rated":
         response = conn.get_top_rated_movies()
-
-    # create dataframe from API response
-    columns = ["title", "release_date", "vote_average", "overview"]
-    # write response 
-    st.write("Top 20 movies:")
-
-    response = conn.get_popular_movies()
-    # print response data type
-    #st.write(response)
-
+    
     if response != None:
         #movies_data = json.loads(response.text)
         movie_info = []
-
-        for movie in response:       
-            title = movie['title']
-            release_date = movie['release_date']
-            vote_average = movie['vote_average']
-            overview = movie['overview']
-            movie_info.append([title, release_date, vote_average, overview])
-            
-        columns = ["Title",  "Release Date","Vote Average", "Overview"]
-        movies_df = pd.DataFrame(movie_info, columns=columns)
-        # Display movies_df
-        st.write(movies_df)
-
+        if movie_type == "Popular":
+            for movie in response:       
+                title = movie['title']
+                release_date = movie['release_date']
+                vote_average = movie['vote_average']
+                overview = movie['overview']
+                movie_info.append([title, release_date, vote_average, overview])
+            columns = ["Title",  "Release Date","Vote Average", "Overview"]
+            movies_df = pd.DataFrame(movie_info, columns=columns)
+            # Display movies_df
+            st.subheader("Popular Movies")
+            st.write(movies_df)
+        elif movie_type == "Top Rated":
+            for movie in response:       
+                title = movie['title']
+                release_date = movie['release_date']
+                vote_average = movie['vote_average']
+                overview = movie['overview']
+                movie_info.append([title, release_date, vote_average, overview])
+            columns = ["Title",  "Release Date","Vote Average", "Overview"]
+            movies_df = pd.DataFrame(movie_info, columns=columns)
+            # Display movies_df
+            st.subheader("Top Rated Movies")
+            st.write(movies_df)
+    
     else:
         st.write("Error: API call failed")
