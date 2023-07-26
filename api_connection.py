4f8df1478d38b0142276bd1fd26771f10fa4310e
Cassandrea Teow <cassteowthg@gmail.com>
2023-07-26 22:29:34 +0800

    debug api

--- api_connection.py
+++ api_connection.py
@@ -2,6 +2,7 @@ import streamlit as st
 from streamlit.connections import ExperimentalBaseConnection
 import requests
 
+@st.cache_data
 def getToken():
     token = st.secrets["token"]
     return token
